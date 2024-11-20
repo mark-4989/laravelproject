5cb7a45e58e817444961d13fcbd6b67dd2ab859e
mark <ngarimark27@gmail.com>
2024-11-20 21:01:53 +0300

    header section

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,50 @@
+<footer>
+    <div class="footer">
+       <div class="container">
+          <div class="row">
+             <div class=" col-md-4">
+                <h3>Contact US</h3>
+                <ul class="conta">
+                   <li><i class="fa fa-map-marker" aria-hidden="true"></i> Address</li>
+                   <li><i class="fa fa-mobile" aria-hidden="true"></i> +01 1234569540</li>
+                   <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
+                </ul>
+             </div>
+             <div class="col-md-4">
+                <h3>Menu Link</h3>
+                <ul class="link_menu">
+                   <li class="active"><a href="#">Home</a></li>
+                   <li><a href="about.html"> about</a></li>
+                   <li><a href="room.html">Our Room</a></li>
+                   <li><a href="gallery.html">Gallery</a></li>
+                   <li><a href="blog.html">Blog</a></li>
+                   <li><a href="contact.html">Contact Us</a></li>
+                </ul>
+             </div>
+           
+          </div>
+       </div>
+       <div class="copyright">
+          <div class="container">
+             <div class="row">
+                <div class="col-md-10 offset-md-1">
+                   
+                   <p>
+                   © 2024 All Rights Reserved
+                   
+                   </p>
+
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+ </footer>
+ <!-- end footer -->
+ <!-- Javascript files-->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.bundle.min.js"></script>
+ <script src="js/jquery-3.0.0.min.js"></script>
+ <!-- sidebar -->
+ <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+ <script src="js/custom.js"></script>
